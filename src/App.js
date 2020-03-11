143218caae5112ab29ea167a95b111a9199a58a9
pridev13 <m@mvhr.nl>
2020-03-11 18:38:37 +0100

    Added checks and sanitizing to input and output

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Options from './components/Options/Options';
 import InputOptions from './components/InputOptions/InputOptions';
 import Display from './components/Display/Display';
 
-import {evaluate} from 'mathjs';
+import { evaluate } from 'mathjs';
 
 const numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
 const calcOptions = [" + ", " - ", " / ", " x "];
@@ -16,10 +16,60 @@ class App extends Component {
     result: ""
   }
 
+  sanitizeInput = (current, input) => {
+
+    let output = "";
+
+    const currentIsEmpty = (
+      current === ''
+      || current === 'NaN'
+      || current === 0
+      || current === "0");
+
+    const hasDot = current.split(' ').slice(-1).toString().search(/\./i) > -1;
+
+    const lastInput = current.trim().replace(" ", "").slice(-1);
+    const lastIsNum = !isNaN(parseFloat(lastInput)) && isFinite(lastInput);
+    const lastIsDot = lastIsNum ? false : lastInput.trim() === '.';
+
+    const inputIsNum = !isNaN(parseFloat(input)) && isFinite(input);
+    const inputIsMinus = inputIsNum ? false : input.trim() === '-';
+    const inputIsDot = inputIsNum ? false : input.trim() === '.';
+
+    if (currentIsEmpty) {
+
+      if (inputIsNum || inputIsMinus) output = input;
+      else if (inputIsDot) output = "0.";
+      else output = current; //input ignored
+
+    }
+    else if (inputIsDot && hasDot) {
+      output = current; //input ignored
+    }
+    else if (lastIsNum) {
+      output = current + "" + input; //all possible
+    }
+    else if (lastIsDot) {
+
+      if (inputIsNum) output = current + "" + input;
+      else output = current; //input ignored
+
+    }
+    else if (inputIsNum || inputIsMinus) {
+      output = current + "" + input;
+    }
+    else {
+      output = current; //input ignored
+    }
+
+    return output.toString();
+
+  }
+
   addToInput = (input) => {
 
     this.setState((prevState) => ({
-      display: prevState.display + "" + input
+      display: this.sanitizeInput(prevState.display, input)
     }));
 
   }
@@ -42,9 +92,18 @@ class App extends Component {
 
   removeLastInput = () => {
 
-    this.setState((prevState) => ({
-      display: prevState.display.slice(0, -1)
-    }));
+    this.setState((prevState) => {
+
+      const newDisplay =
+        prevState.display.slice(-1) === ' '
+          ? prevState.display.trim().slice(0, -1).trim()
+          : prevState.display.slice(0, -1)
+
+      return {
+        display: newDisplay
+      }
+
+    });
 
   }
 
@@ -60,6 +119,20 @@ class App extends Component {
 
   doCalc = (input) => {
 
+    const inputIsEmpty = (
+      input === ''
+      || input === 'NaN'
+      || input === 0
+      || input === "0");
+
+    const elements = input.split(' ');
+    const lastIsNum = !isNaN(parseFloat(elements.slice(-1))) && isFinite(elements.slice(-1));
+    const lastIsDot = input.toString().trim().slice(-1) === '.';
+
+    if(inputIsEmpty || elements.length < 3 || !lastIsNum || lastIsDot) {
+      return false;
+    }
+
     let result = 'NaN';
 
     input = input.replace(" ", "");
@@ -67,9 +140,8 @@ class App extends Component {
 
     try {
       result = evaluate(input);
-      
-    }catch (error) {
-      console.log('Error occured') ;
+    } catch (error) {
+      console.log('Error occured');
     }
 
     return result;
@@ -80,9 +152,17 @@ class App extends Component {
 
     const result = this.doCalc(this.state.display);
 
+    let newDisplay = this.state.display;
+    let newResult = this.state.result;
+
+    if(result !== false) {
+      newDisplay = result.toString();
+      newResult = this.state.display + " = " + result;
+    }
+
     this.setState({
-      display: result.toString(),
-      result: this.state.display + " = " + result
+      display: newDisplay,
+      result: newResult
     });
 
     console.log('Result calculated');
